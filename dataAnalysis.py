8bb5b1b9058cd778c532fec3e4086b7da9d8b493
FFFFly-Fish <82267318+FFFFly-Fish@users.noreply.github.com>
2022-04-16 19:47:15 +0800

    1.0.0

---
+++ dataAnalysis.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import jieba
+
+# 读取文件
+data = pd.read_csv('CleanData_ColaC.csv')
+df = pd.DataFrame(data)
+user = df['nickname']  # 获取昵称列
+text = df['text']  # 获取内容列
+
+# 用户发言数量统计
+user_result = user.value_counts()
+user_result.to_csv('user_result.csv', encoding='utf-8')
+
+# 关键词频率统计
+text_str = text.str.cat(sep=' ')  # 将dataframe数据转换为str
+text_cut = jieba.lcut(text_str)  # 使用jieba进行分词
+
+counts = {}
+for i in text_cut:
+    if len(i) > 1:
+        counts[i] = counts.get(i, 0) + 1  # 选择出非单字
+
+text_result = list(counts.items())
+text_result.sort(key=lambda x: x[1], reverse=True)  # 从大到小排序
+
+column_name = ['keywords', 'times']
+text_df = pd.DataFrame(columns=column_name, data=text_result)  # 转换回dataframe数据并保存
+text_df.to_csv('text_result.csv', encoding='utf-8')
+
+print(user_result)
+print(text_df)
