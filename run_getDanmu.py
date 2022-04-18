e746bd3ff3967436ea1f1c6ffd10b7d4e218024e
FFFFly-Fish <82267318+FFFFly-Fish@users.noreply.github.com>
2022-04-18 18:43:47 +0800

    加入了配置文件接口

--- run_getDanmu.py
+++ run_getDanmu.py
@@ -1,15 +1,22 @@
 import subprocess
 import time
 import func_timeout
+import configparser
+
+# 读取配置文件设置参数
+config = configparser.ConfigParser()
+config.read("config.ini", encoding='utf-8')
+runtime = int(config["Runtime"]["second"])
+sleeptime = int(config["Sleeptime"]["second"])
 
 
 # 运行爬虫程序
-@func_timeout.func_set_timeout(120)  # 设置运行时间（秒），时间结束后结束进程
+@func_timeout.func_set_timeout(runtime)  # 设置运行时间（秒），时间结束后结束进程
 def GetAndClean():
     while True:
         subprocess.run('python getDanmu.py')  # 运行写入程序，将获取的弹幕写入文件
         subprocess.run('python dataClean.py')  # 运行去重程序
-        time.sleep(1)  # 设置一定时间间隔（秒）
+        time.sleep(sleeptime)  # 设置一定时间间隔（秒）
 
 
 # 运行分析与可视化程序
